4f1d4e055328c052c8d1349b36b55f4ff0507976
Velia Precious <veliaprecious@gmail.com>
2024-07-09 19:57:31 +0100

    add

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -333,8 +333,131 @@ const Cart = () => {
                   </ul>
                 </div>
 
-                <div>
-                  <p>Similar items you may like</p>
+                <div className="p-5">
+                  <h3 className="text-xl font-semibold text-start">
+                    Similar items you may like
+                  </h3>
+
+                  <div className="cards w-full my-5 flex items-center gap-4">
+                    <div className="card w-[30%] min-w-[300px]">
+                      <div className="border-2 rounded-lg border-mediumGray p-4 w-full">
+                        <div className="space-y-2">
+                          <div className="bg-lightGray rounded-lg ">
+                            <div className="flex justify-between p-4">
+                              <div className="bg-slate-50 p-2 rounded-full flex">
+                                <img
+                                  className="h-[90%] w-[90%] self-center"
+                                  src={Heart}
+                                  alt="hear-icon"
+                                />
+                              </div>
+
+                              <div className="bg-slate-50 px-[10px] py-[5px] rounded-3xl text-primaryGray">
+                                <p>In Stock</p>
+                              </div>
+                            </div>
+                            <div className="w-full flex justify-center items-center mt-0 pt-0">
+                              <img
+                                src={Bag}
+                                alt="bag-icon"
+                                className="self-center"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="flex justify-between items-center">
+                            <div className="border border-mediumGray px-[10px] py-[5px] rounded-3xl text-mediumGray">
+                              <p>women</p>
+                            </div>
+                            <div className="flex text-primaryGray">
+                              <span>
+                                <img src={PinkShoe} alt="star" />
+                              </span>
+                              <p>2k rating</p>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="card-bottom flex justify-between mt-4">
+                          <div className="space-y-5">
+                            <div>
+                              <h3 className="text-primaryGray font-bold text-lg">
+                                French Kiss Bag
+                              </h3>
+                              <p className="text-mediumGray">ALDO</p>
+                            </div>
+                            <h3 className="text-primaryGray font-bold text-lg">
+                              $500
+                            </h3>
+                          </div>
+                          <div className="self-end">
+                            <button className="bg-primaryGray text-slate-50 py-3 px-6 rounded-xl">
+                              Add to Cart
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="card w-[30%] min-w-[300px]">
+                      <div className="border-2 rounded-lg border-mediumGray p-4 w-full">
+                        <div className="space-y-2">
+                          <div className="bg-lightGray rounded-lg ">
+                            <div className="flex justify-between p-4">
+                              <div className="bg-slate-50 p-2 rounded-full flex">
+                                <img
+                                  className="h-[90%] w-[90%] self-center"
+                                  src={Heart}
+                                  alt="hear-icon"
+                                />
+                              </div>
+
+                              <div className="bg-slate-50 px-[10px] py-[5px] rounded-3xl text-primaryGray">
+                                <p>In Stock</p>
+                              </div>
+                            </div>
+                            <div className="w-full flex justify-center items-center mt-0 pt-0">
+                              <img
+                                src={Bag}
+                                alt="bag-icon"
+                                className="self-center"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="flex justify-between items-center">
+                            <div className="border border-mediumGray px-[10px] py-[5px] rounded-3xl text-mediumGray">
+                              <p>women</p>
+                            </div>
+                            <div className="flex text-primaryGray">
+                              <span>
+                                <img src={PinkShoe} alt="star" />
+                              </span>
+                              <p>2k rating</p>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="card-bottom flex justify-between mt-4">
+                          <div className="space-y-5">
+                            <div>
+                              <h3 className="text-primaryGray font-bold text-lg">
+                                French Kiss Bag
+                              </h3>
+                              <p className="text-mediumGray">ALDO</p>
+                            </div>
+                            <h3 className="text-primaryGray font-bold text-lg">
+                              $500
+                            </h3>
+                          </div>
+                          <div className="self-end">
+                            <button className="bg-primaryGray text-slate-50 py-3 px-6 rounded-xl">
+                              Add to Cart
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
